007b1275f2cffdf523421ed6c4fe6736124f73ac
Suraj Bodapati <surajbodapati@Surajs-MacBook-Air.local>
2021-09-21 23:01:14 -0400

    replys commented for testing

--- tweet_preprocessor.py
+++ tweet_preprocessor.py
@@ -42,8 +42,8 @@ def createDictionary(tweet,country,twitter_class_object,poi_name_flag):
     formatted_date=_get_tweet_date(tweet.created_at)
 
     #Fetching reply tweets
-#     reply_tweet=''
-    reply_tweet=fetch_reply_tweet(tweet,twitter_class_object)
+    reply_tweet=''
+#     reply_tweet=fetch_reply_tweet(tweet,twitter_class_object)
 
     #Setting name and id - for POI and Non-POI
     poi_name=''
