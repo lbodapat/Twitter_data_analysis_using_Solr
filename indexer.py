2c9c5d13730f4ca19355427f66595ee0471d6c1b
Suraj Bodapati <surajbodapati@Surajs-MacBook-Air.local>
2021-09-22 14:17:04 -0400

    s

--- indexer.py
+++ indexer.py
@@ -1,3 +1,8 @@
+'''
+@author: Souvik Das
+Institute: University at Buffalo
+'''
+
 import os
 import pysolr
 import requests
@@ -5,8 +10,7 @@ import requests
 # https://tecadmin.net/install-apache-solr-on-ubuntu/
 
 
-CORE_NAME = "IRF21P1_nr2"
-# CORE_NAME = "Dummy123"
+CORE_NAME = "IRF21P1"
 AWS_IP = "localhost"
 
 
@@ -14,7 +18,8 @@ AWS_IP = "localhost"
 
 
 def delete_core(core=CORE_NAME):
-    print(os.system('sudo su - solr -c "/opt/solr/bin/solr delete -c {core}"'.format(core=core)))
+    print(os.system(
+        'sudo su - solr -c "/opt/solr/bin/solr delete -c {core}"'.format(core=core)))
 
 
 def create_core(core=CORE_NAME):
@@ -26,7 +31,8 @@ def create_core(core=CORE_NAME):
 class Indexer:
     def __init__(self):
         self.solr_url = f'http://{AWS_IP}:8983/solr/'
-        self.connection = pysolr.Solr(self.solr_url + CORE_NAME, always_commit=True, timeout=5000000)
+        self.connection = pysolr.Solr(
+            self.solr_url + CORE_NAME, always_commit=True, timeout=5000000)
 
     def do_initial_setup(self):
         delete_core()
@@ -36,93 +42,133 @@ class Indexer:
         print(self.connection.add(docs))
 
     def add_fields(self):
-        '''
-        Define all the fields that are to be indexed in the core. Refer to the project doc for more details
-        :return:
-        '''
         data = {
-                    "add-field": [
-                        {
-                            "name": "poi_name",
-                            "type": "string",
-                            "multiValued": False
-                        },
-                        {
-                            "name": "poi_id",
-                            "type": "plong",
-                            "multiValued": False
-                        }, {
-                            "name": "verified",
-                            "type": "boolean",
-                            "multiValued": False
-                        },
-                        {
-                            "name": "country",
-                            "type": "string",
-                            "multiValued": False
-                        },{
-                            "name": "replied_to_tweet_id",
-                            "type": "plong",
-                            "multiValued": False
-                        },{
-                            "name": "replied_to_user_id",
-                            "type": "plong",
-                            "multiValued": False
-                        },{
-                            "name": "reply_text",
-                            "type": "text_general",
-                            "multiValued": False
-                        },{
-                            "name": "tweet_text",
-                            "type": "text_general",
-                            "multiValued": False
-                        },{
-                            "name": "tweet_lang",
-                            "type": "string",
-                            "multiValued": False
-                        },{
-                            "name": "text_en",
-                            "type": "text_general",
-                            "multiValued": False
-                        },{
-                            "name": "text_hi",
-                            "type": "text_general",
-                            "multiValued": False
-                        },{
-                            "name": "text_es",
-                            "type": "text_general",
-                            "multiValued": False
-                        },{
-                            "name": "hashtags",
-                            "type": "strings",
-                            "multiValued": True
-                        },{
-                            "name": "mentions",
-                            "type": "strings",
-                            "multiValued": True
-                        },{
-                            "name": "tweet_urls",
-                            "type": "strings",
-                            "multiValued": True
-                        },{
-                            "name": "tweet_emoticons",
-                            "type": "strings",
-                            "multiValued": True
-                        },{
-                            "name": "tweet_date",
-                            "type": "pdate",
-                            "multiValued": False
-                        },{
-                            "name": "geolocation",
-                            "type": "string",
-                            "multiValued": False
-                        }
-                    ]
+            "add-field": [
+                {
+                    "name": "poi_name",
+                    "type": "string",
+                    "multiValued": False
+                },
+                {
+                    "name": "poi_id",
+                    "type": "plong",
+                    "multiValued": False
+                }, {
+                    "name": "verified",
+                    "type": "boolean",
+                    "multiValued": False
+                },
+                {
+                    "name": "country",
+                    "type": "string",
+                    "multiValued": False
+                },
+                # {
+                #     "name": "id",
+                #     "type": "string",
+                #     "multiValued": False
+                # },
+                {
+                    "name": "replied_to_tweet_id",
+                    "type": "plong",
+                    "multiValued": False
+                },
+                {
+                    "name": "replied_to_user_id",
+                    "type": "plong",
+                    "multiValued": False
+                },
+                {
+                    "name": "reply_text",
+                    "type": "text_general",
+                    "multiValued": False
+                },
+                {
+                    "name": "tweet_text",
+                    "type": "text_general",
+                    "multiValued": False
+                },
+                {
+                    "name": "tweet_lang",
+                    "type": "string",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_en",
+                    "type": "text_en",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_hi",
+                    "type": "text_hi",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_es",
+                    "type": "text_es",
+                    "multiValued": False
+                },
+                {
+                    "name": "hashtags",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "mentions",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_urls",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_emoticons",
+                    "type": "strings",
+                    "multiValued": True
+                },
+                {
+                    "name": "tweet_date",
+                    "type": "pdate",
+                    "multiValued": False
+                },
+                {
+                    "name": "geolocation",
+                    "type": "string",
+                    "multiValued": False
                 }
+            ]
+        }
 
         print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
+    def replace_fields(self):
+        data = {
+            "replace-field": [
+                {
+                    "name": "text_en",
+                    "type": "text_en",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_hi",
+                    "type": "text_hi",
+                    "multiValued": False
+                },
+                {
+                    "name": "text_es",
+                    "type": "text_es",
+                    "multiValued": False
+                }
+            ]
+        }
+
+        print(requests.post(self.solr_url + CORE_NAME + "/schema", json=data).json())
+
 
 if __name__ == "__main__":
     i = Indexer()
     i.do_initial_setup()
     i.add_fields()
+    #i.replace_fields()
